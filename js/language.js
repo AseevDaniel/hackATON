2e49152a8f3810f87108a2387cb4b7457e6798fe
Aseev Daniel <aseev.doonel@gmail.com>
2023-02-18 21:56:29 +0200

    mobile + modals

--- js/language.js
+++ js/language.js
@@ -3,3 +3,7 @@ const switcher = document.getElementById('languageSwitcher')
 function onSwitcherClick(){
     switcher.classList.toggle('active')
 }
+
+function onChangeLanguage(){
+    location.reload()
+}
